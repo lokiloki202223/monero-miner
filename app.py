3a6057a556d6937f946e457535279fc84ff7b22c
vyom907 <88526180+vyom907@users.noreply.github.com>
2022-05-22 19:28:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+
+import miner
 import streamlit as st
 import pandas as pd
 import numpy as np
+st.title('monero miner on stream lit')
 
 i = 0
 
-while i == 0 : st.write('Hello, *World!* :sunglasses:')
+if i == 0 :
+  st.write('*miner stopped*')
+end if
+if i == 1 :
+  st.write('*miner started*')
+  end if
+  
+  
 
