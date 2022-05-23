d403de7c7c205583478fa6e1b0ab165c1cec424c
vyom907 <88526180+vyom907@users.noreply.github.com>
2022-05-23 15:28:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import subprocess
 import streamlit as st
 import pandas as pd
 import numpy as np
-import miner.py
-st.title('monero miner on stream lit')
+import https://raw.githubusercontent.com/vyom907/monero-miner/main/miner.py
+st.title('monero miner on streamlit')
 
 
 
