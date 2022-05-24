380e81139624c83d62dd6e8f4c5b305d042b637c
vyom907 <88526180+vyom907@users.noreply.github.com>
2022-05-24 11:00:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-#import cmake
-import sys
-import subprocess
-import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('monero miner on streamlit')
-
-
-
-  subprocess.call("miner.py", shell=True)
-  
 
 
 
